4414b78055578dd313636d37f7f98660bd78203b
Everton Zoelner <everton.zoelner@gmail.com>
2019-07-31 00:03:56 -0300

    Reactotron

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React, { Fragment } from 'react';
 import { StatusBar } from 'react-native';
+
+import '~/config/ReactotronConfig';
+
 import Routes from './routes';
 
 export default function App() {
